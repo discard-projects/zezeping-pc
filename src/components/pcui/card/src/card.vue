5ac2b3dfe1193bea79aa131273beba4703734b52
mars <578595193@qq.com>
2018-03-08 15:51:07 +0800

    FIX

--- src/components/pcui/card/src/card.vue
+++ src/components/pcui/card/src/card.vue
@@ -28,7 +28,6 @@ export default {
     border-radius: 4px;
     border: 1px solid #ebeef5;
     background-color: #fff;
-    overflow: hidden;
     box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
     color: #303133;
 
