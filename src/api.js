395ce5d77a13d9ffcbd1feba18bc3a8e05727cc2
mars <578595193@qq.com>
2018-03-16 13:26:58 +0800

    data encryption

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
+const rotten = require('rotten-encryption2')('zezeping', true)
 let store = null
 const cusAxios = axios.create({
   baseURL: process.env.API_ROOT,
@@ -24,6 +25,12 @@ cusAxios.interceptors.request.use(function (config) {
 // Add a response interceptor
 cusAxios.interceptors.response.use(function (response) {
   Vue.prototype.$loadingBar.finish()
+  try {
+    response.data = rotten.decode(response.data)
+    response.data = JSON.parse(response.data)
+  } catch (err) {
+    console.log(err)
+  }
   // Do something with response data
   if (response.data.msg) {
     Vue.prototype.$message({ showClose: true, message: response.data.msg, type: 'success' })
@@ -33,6 +40,12 @@ cusAxios.interceptors.response.use(function (response) {
   return response
 }, function (error) {
   Vue.prototype.$loadingBar.error()
+  try {
+    error.response.data = rotten.decode(error.response.data)
+    error.response.data = JSON.parse(error.response.data)
+  } catch (err) {
+    console.log(err)
+  }
   // Do something with response error
   if (error.response) {
     if (error.response.status === 403) {
